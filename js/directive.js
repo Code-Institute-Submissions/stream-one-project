556099366651fc133791a7075809ca620ee32266
n-peaches <nicolepeach84@gmail.com>
2017-03-09 16:24:52 +0000

    Project Updates

--- js/directive.js
+++ js/directive.js
@@ -1,12 +1,4 @@
 angular.module('bandAppDirectives', [])
-	.directive('imageCarousel', function(){
-		return{
-			restrict: 'E',
-			scope: true,
-			templateUrl: 'templates/directives/carousel.html'
-		};
-	})
-
 	.directive('musicTable', function(){
 		return{
 			restrict: 'EA',
